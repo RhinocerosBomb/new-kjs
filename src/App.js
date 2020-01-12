edde0b12869a55431a13d8481406a63770147de8
Jason <rhinocerosbomb@gmail.com>
2020-01-11 22:51:22 -0500

    Revert "edit app"
    This reverts commit 8fddfee943c6e5d486e792b3cac1988c47cb9609.

--- src/App.js
+++ src/App.js
@@ -10,9 +10,6 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p> 
-          kasra jannati : 101237299
-          </p>
         <a
           className="App-link"
           href="https://reactjs.org"
